2c67d58189bd24fd31a3f60b89d23dae9074fbcf
Tao Liao <taoliao@google.com>
2021-03-15 13:35:53 -0400

    Vertical segment the vectors.

--- combine/main_combine.go
+++ combine/main_combine.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"flag"
 	"math/rand"
+	"path"
 	"reflect"
+	"strings"
 
 	"github.com/apache/beam/sdks/go/pkg/beam"
 	"github.com/apache/beam/sdks/go/pkg/beam/log"
@@ -17,7 +19,7 @@ var (
 	inputFile = flag.String("input_file", "", "Input file.")
 	outputFile = flag.String("output_file", "", "Output file.")
 	logN = flag.Uint64("log_n", 8, "Vector size bit.")
-	combineDomain = flag.Uint64("combine_domain", 100000, "Number of keys for pre-combine.")
+	segmentBits = flag.Uint64("segment_bits", 14, "Length of the vec segments.")
 )
 
 func init() {
@@ -129,6 +131,70 @@ func (fn *flattenVecFn) ProcessElement(ctx context.Context, vec pairedVec, emit 
 	return nil
 }
 
+type combineVecRangeFn struct {
+	StartIndex uint64
+	Length uint64
+
+	inputCounter beam.Counter
+	createCounter beam.Counter
+	mergeCounter beam.Counter
+}
+
+func (fn *combineVecRangeFn) Setup() {
+	fn.inputCounter = beam.NewCounter("aggregation","combineVecRangeFn-input-count")
+	fn.createCounter = beam.NewCounter("aggregation","combineVecRangeFn-create-count")
+	fn.mergeCounter = beam.NewCounter("aggregation","combineVecRangeFn-merge-count")
+}
+
+func (fn *combineVecRangeFn) CreateAccumulator(ctx context.Context) pairedVec {
+	fn.createCounter.Inc(ctx, 1)
+
+	return pairedVec{Vec1: make([]uint64, fn.Length), Vec2: make([]uint64, fn.Length)}
+}
+
+func (fn *combineVecRangeFn) AddInput(ctx context.Context, e pairedVec, p pairedVec) pairedVec {
+	fn.inputCounter.Inc(ctx, 1)
+
+	for i := uint64(0); i < fn.Length; i++ {
+		e.Vec1[i] += p.Vec1[i + fn.StartIndex]
+		e.Vec2[i] += p.Vec2[i + fn.StartIndex]
+	}
+	return e
+}
+
+func (fn *combineVecRangeFn) MergeAccumulators(ctx context.Context, a, b pairedVec) pairedVec {
+	fn.mergeCounter.Inc(ctx, 1)
+
+	for i := uint64(0); i < fn.Length; i++ {
+		a.Vec1[i] += b.Vec1[i]
+		a.Vec2[i] += a.Vec2[i]
+	}
+	return a
+}
+
+type flattenVecRangeFn struct {
+	StartIndex uint64
+
+	counterInput beam.Counter
+	counterOutput beam.Counter
+}
+
+func (fn *flattenVecRangeFn) Setup(ctx context.Context) {
+	fn.counterInput = beam.NewCounter("aggregation","flattenVecRangeFn_input_count")
+	fn.counterOutput = beam.NewCounter("aggregation","flattenVecRangeFn_output_count")
+}
+
+func (fn *flattenVecRangeFn) ProcessElement(ctx context.Context, vec pairedVec, emit func(string)) error {
+	fn.counterInput.Inc(ctx, 1)
+
+	l := len(vec.Vec1)
+	for i := 0; i < l; i++ {
+		fn.counterOutput.Inc(ctx, 1)
+		emit(fmt.Sprintf("%d,%d,%d", uint64(i) + fn.StartIndex, vec.Vec1[i], vec.Vec2[i]))
+	}
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
@@ -142,15 +208,14 @@ func main() {
 
 	vecs := beam.ParDo(scope, &genVecFn{LogN: *logN}, rRecords)
 
-	keyVecs := beam.ParDo(scope, &addRandomKeyFn{Domain: *combineDomain}, vecs)
-	combinedKeyVecs := beam.CombinePerKey(scope, &combineVecFn{LogN: *logN}, keyVecs)
-	combinedVecs := beam.DropKey(scope, combinedKeyVecs)
-
-	histogram := beam.Combine(scope, &combineVecFn{LogN: *logN}, combinedVecs)
-
-	lines := beam.ParDo(scope, &flattenVecFn{}, histogram)
-	textio.Write(scope, *outputFile, lines)
-
+	totalLength := uint64(1 << *logN)
+	segLength := uint64(1 << *segmentBits)
+	for i := uint64(0); i < totalLength/segLength; i++ {
+		fileName := strings.ReplaceAll(*outputFile, path.Ext(*outputFile), fmt.Sprintf("-%d-%d%s", i+1, totalLength/segLength, path.Ext(*outputFile)))
+		pHistogram := beam.Combine(scope, &combineVecRangeFn{StartIndex: i * segLength, Length: segLength}, vecs)
+		flattened := beam.ParDo(scope, &flattenVecRangeFn{StartIndex: i * segLength}, pHistogram)
+		textio.Write(scope, fileName, flattened)
+	}
 	if err := beamx.Run(ctx, pipeline); err != nil {
 		log.Exitf(ctx, "Failed to execute job: %s", err)
 	}
